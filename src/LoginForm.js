69cecf70393922eec182bb7ec9372d7c460e8594
Soniya Kambli <kamblisoniya@gmail.com>
2023-10-22 09:50:32 +0530

    modified:   src/LoginForm.js

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -8,7 +8,7 @@ const LoginForm = () => {
             <input type="text" placeholder='Enter your email' className='w-full border rounded-md bg-transparent border-gray-400 p-3'/>
         </div>
         <div className='mb-3'>
-            <label className='font-medium mb-2 flex'>Email</label>
+            <label className='font-medium mb-2 flex'>Password</label>
             <input type="tepassword" placeholder='Enter your password' className='w-full border rounded-md bg-transparent border-gray-400 p-3'/>
         </div>
         <div className='flex justify-between mb-6'>
@@ -16,7 +16,7 @@ const LoginForm = () => {
                 <input type='checkbox' className='mr-2'/>
                 Remember me
             </label>
-            <span>Forgot password</span>
+            <span>Forgot password?</span>
 
         </div>
         <button className='block bg-blue-700 hover:bg-blue-800 text-white w-full py-2 px rounded '>Login</button>
